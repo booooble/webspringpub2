13624ccf0158640c10176e61b8c8fd3d78bdcf73
Anton <tohalitv@gmail.com>
2016-05-21 22:34:13 +0300

    refactoring pub and controller

--- src/main/java/springinthepub/Pub.java
+++ src/main/java/springinthepub/Pub.java
@@ -20,7 +20,149 @@ public class Pub {
     protected double drunkBeer = 0;
     private double relativeCapacity = 0;
     public StringBuffer historyText = new StringBuffer();
+    public static final String EMPTY_BAR_NOTIFICATION = "There is noone to be removed... Add someone!\n";
+    public static final String EMPTY_QUEUE_NOTIFICATION = "The queue is empty. Waiting for visitors...\n";
+    public static final String OUT_OF_BEER_NOTIFICATION = "The beerlimit has been ended. Composing the order...\n";
+    public static final String PARTY_IS_OVER_NOTIFICATION = "The order has been rejected. The party is over...\n";
 
+    @Autowired
+    public Pub(String pubName, double maxBeerLimit) {
+        this.pubName = pubName;
+        this.maxBeerLimit = maxBeerLimit;
+        this.beerLiterLimit = maxBeerLimit;
+    }
+    
+    public void addRandomVisitorToBar() throws IOException, InterruptedException {
+    	addVisitorToBar(RandomGenerator.personRandomGenerator());
+    }
+    
+    public void addVisitorToBar(Beerman visitor) throws IOException, InterruptedException {    	
+    	if (isBarFull()){
+            visitorsQueue.addLast(visitor);
+            increaseQueueSize();
+    		historyText.append(generateBarIsFullNotification(visitor));
+    	}
+    	else{    		
+    		if(isVisitorEligible(visitor)){
+    			if(!isEnoughBeer(visitor)){
+        			makeAnOrder();
+        		}
+    			visitors.add(visitor);
+    			updateAfterAddingToBar(visitor);
+    		}
+    		else{
+    			notifyRejectionReason(visitor);
+    		}
+    	}  	
+    }
+    
+    public void updateAfterAddingToBar(Beerman visitor){
+    	historyText.append(generateCameInNotification(visitor));
+        currCapacity++;
+        beerLiterLimit -= visitor.getLitersToDrink();
+        drunkBeer += visitor.getLitersToDrink();
+        updateRelativeCapacity();
+        Beerman.beerManCount++;
+    }
+    
+    public void addRandomVisitorToTheQueue() throws IOException {
+    	Beerman visitor = RandomGenerator.personRandomGenerator();
+        visitorsQueue.addLast(visitor);
+        increaseQueueSize();
+        historyText.append(visitor.getName() + " has came and is waiting at the end of the queue.\n");
+    }
+    
+    public void processTheQueue() {
+        if (visitorsQueue.isEmpty()) {
+        	historyText.append(EMPTY_QUEUE_NOTIFICATION);      	
+        }
+        else {
+            while (!visitorsQueue.isEmpty()) {
+                Beerman visitor = visitorsQueue.pollFirst();
+                if (!isEnoughBeer(visitor)) {
+                    makeAnOrder();
+                }
+                if (isVisitorEligible(visitor) && !isBarFull()) {
+                	visitors.add(visitor);
+                	updateAfterAddingToBar(visitor);
+                }                 
+                else {
+                	if(isBarFull()){
+                		visitorsQueue.addFirst(visitor);
+                		historyText.append(generateBarIsFullNotification(visitor));         		
+                		break;
+                	}
+                	else {
+                		notifyRejectionReason(visitor);
+                	}
+                }
+            }
+        }
+    }
+    
+    public void removeVisitor(){
+    	if(visitors.size() > 0){
+    		String removedVisitorName = visitors.get(0).getName();
+    		visitors.remove(0);
+    		updateAfterRemoving(removedVisitorName);
+    	}
+    	else{
+    		historyText.append(EMPTY_BAR_NOTIFICATION);
+    	}
+    }
+    
+    public void updateAfterRemoving(String visitorName){
+		currCapacity--;
+		updateRelativeCapacity();
+		historyText.append(visitorName + " has been removed.\n");
+    }
+    
+    public boolean makeAnOrder() {
+    	historyText.append(OUT_OF_BEER_NOTIFICATION);
+    	double order = this.maxBeerLimit - this.beerLiterLimit;
+        historyText.append("The order for " + order + " liters was sent to the provider...\n");
+        boolean isOrderReceived = Provider.sendOrder(order);
+        if (isOrderReceived) {
+        	historyText.append("We ordered " + order + " liters successfully. The party is going on!\n");
+        	increaseBeerLimit(order);
+            return true;
+        } else {
+        	historyText.append(PARTY_IS_OVER_NOTIFICATION);
+            return false;
+        }
+    }
+    
+    public Boolean isEnoughBeer(Beerman visitor){
+    	return beerLiterLimit - visitor.getLitersToDrink() >= 0;
+    }
+    
+    public Boolean isBarFull(){
+    	return currCapacity == maxCapacity;
+    }
+    
+    public Boolean isVisitorEligible(Beerman visitor){
+    	return visitor.age >= 18;
+    }
+    
+    public void notifyRejectionReason(Beerman visitor){
+    	historyText.append(visitor.getName() + " was rejected ");           
+		if (!isVisitorEligible(visitor)) {
+			historyText.append("(" + visitor.getAge() + " years old).\n");       	
+		}
+		else{
+			historyText.append("(For unknown reason).\n");
+		}
+    }
+    
+    public String generateCameInNotification(Beerman visitor){
+    	return visitor.getName() + " came in. And drunk " + visitor.litersToDrink + " liter(s).\n";
+    }
+    
+    public String generateBarIsFullNotification(Beerman visitor){
+    	return "No way! The Bar is full!!! " + visitor.getName() + 
+    			" is not  able to come in! Ask someone to leave! This visitor is waiting in the queue!\n";
+    }
+  
     public double getRelativeCapacity() {
 		return relativeCapacity;
 	}
@@ -30,7 +172,7 @@ public class Pub {
 	}
 
 	public void updateRelativeCapacity() {
-		this.relativeCapacity = (double) this.currCapacity / this.maxCapacity;
+		this.relativeCapacity = (double) currCapacity / maxCapacity;
 	}
 	
 	public int getMaxCapacity() {
@@ -68,14 +210,7 @@ public class Pub {
     public void increaseCurrCapacity(){
         currCapacity++;
     }
-
-    @Autowired
-    public Pub(String pubName, double maxBeerLimit) {
-        this.pubName = pubName;
-        this.maxBeerLimit = maxBeerLimit;
-        this.beerLiterLimit = maxBeerLimit;
-    }
-    
+   
     public StringBuffer getHistoryText() {
 		return historyText;
 	}
@@ -84,111 +219,12 @@ public class Pub {
 		this.historyText = historyText;
 	}
 
-	public Pub() {
-    }
-
     public List<Beerman> getVisitors() {
         return visitors;
     }
 
     public int getVisitorsQueueSize() {
         return visitorsQueue.size();
-    }
-
-    public void addRandomVisitorToTheQueue() throws IOException {
-    	Beerman vis = RandomGenerator.personRandomGenerator();
-        this.visitorsQueue.addLast(vis);
-        increaseQueueSize();
-        this.historyText.append(vis.getName() + " has came and is waiting at the end of the queue.\n");
-    }
-    
-    public void addRandomVisitor() throws IOException {
-    	
-    	if (currCapacity == maxCapacity){
-    		this.historyText.append("The Bar is full!!! Ask someoone to leave! The visitor is still in the queue!\n");
-    	}
-    	else{    		
-    		Beerman vis = RandomGenerator.personRandomGenerator();
-    		boolean isEnoughBeer = this.beerLiterLimit - vis.getLitersToDrink() >= 0;    		
-    		if(vis.age > 18){
-    			if(!isEnoughBeer){
-        			makeAnOrder();
-        		}
-    			this.visitors.add(vis);
-    			this.historyText.append(vis.getName() + " came in. And drunk " + vis.litersToDrink + " liter(s).\n");
-                this.currCapacity++;
-                this.beerLiterLimit -= vis.getLitersToDrink();
-                this.drunkBeer += vis.getLitersToDrink();
-                this.updateRelativeCapacity();
-                Beerman.beerManCount++;
-    		}
-    		else{this.historyText.append(vis.getName() + " was rejected ");           
-    			if (vis.getAge() < 18) {
-    				this.historyText.append("(" + vis.getAge() + " years old).\n");
-        	
-    			}
-    		}
-    	}
-
-    	
-    }
-    
-    public void removeVisitor(){
-    	if(this.visitors.size() > 0){
-    		String removedVisitorName = visitors.get(0).getName();
-    		this.visitors.remove(0);
-    		this.currCapacity--;
-    		updateRelativeCapacity();
-    		this.historyText.append(removedVisitorName + " has been removed.\n");
-    	}
-    	else{
-    		this.historyText.append("There is noone to be removed... Add someone!\n");
-    	}
-    }
-
-    public void userFilter() {
-        if (visitorsQueue.isEmpty()) {
-        	this.historyText.append("The queue is empty. Waiting for visitors...\n");      	
-        }
-        else if (currCapacity == maxCapacity){
-    		this.historyText.append("The Bar is full!!! Ask someoone to leave! The visitor is still in the queue!\n");
-    	}
-
-        else {
-            while (!visitorsQueue.isEmpty()) {
-                boolean isEnoughBeer = this.beerLiterLimit - visitorsQueue.getFirst().getLitersToDrink() >= 0;
-                if (!isEnoughBeer) {
-                    makeAnOrder();
-                }
-                Beerman visitor = visitorsQueue.pollFirst();
-                if (visitor.age >= 18 && this.currCapacity < maxCapacity) {
-                	visitors.add(visitor);
-                	this.historyText.append(visitor.getName() + " came in. And drunk " + visitor.litersToDrink + " liter(s).\n");
-                    this.currCapacity++;
-                    this.beerLiterLimit -= visitor.getLitersToDrink();
-                    this.drunkBeer += visitor.getLitersToDrink();
-                    this.updateRelativeCapacity();
-                    Beerman.beerManCount++;
-                }                 
-                else {
-                	if(currCapacity == maxCapacity){
-                		visitorsQueue.addFirst(visitor);
-                		this.historyText.append(visitor.getName() + " is not able to come in because the bar is full). "
-                				+ "This visitor is still waiting in the queue...\n");         		
-                		break;
-                	}
-                	else {this.historyText.append(visitor.getName() + " was rejected ");           
-                		if (visitor.getAge() < 18) {
-                			this.historyText.append("(" + visitor.getAge() + " years old).\n");
-                    	}
-
-                		else {
-                			this.historyText.append("(The reason is unknown).\n");
-                		}
-                	}
-                }
-            }
-        }
     }
 
     public void increaseBeerLimit(double order){
@@ -199,29 +235,6 @@ public class Pub {
     	this.beerLiterLimit -= minus;
     }
     
-    public boolean makeAnOrder() {
-    	this.historyText.append("The beerlimit has been ended. Composing the order...\n");
-        double order = this.maxBeerLimit - this.beerLiterLimit;
-        this.historyText.append("The order for " + order + " liters was sent to the provider...\n");
-        boolean isOrderReceived = Provider.sendOrder(order);
-        if (isOrderReceived) {
-        	this.historyText.append("We ordered " + order + " liters successfully. The party is going on!\n");
-        	increaseBeerLimit(order);
-            return true;
-        } else {
-        	this.historyText.append("The order has been rejected. The party is over...\n");
-            return false;
-        }
-    }
-
-    @Override
-	public String toString() {
-		return "Pub [maxCapacity=" + maxCapacity + ", pubName=" + pubName + ", currCapacity=" + currCapacity
-				+ ", beerLiterLimit=" + beerLiterLimit + ", maxBeerLimit=" + maxBeerLimit + ", visitorsQueue="
-				+ visitorsQueue + ", visitors=" + visitors + ", visitorsQueueSize=" + visitorsQueueSize + ", drunkBeer="
-				+ drunkBeer + "]";
-	}
-
 	public String getPubName() {
         return pubName;
     }
@@ -269,6 +282,14 @@ public class Pub {
 
 	public void setDrunkBeer(double drunkBeer) {
 		this.drunkBeer = drunkBeer;
+	}
+	
+    @Override
+	public String toString() {
+		return "Pub [maxCapacity=" + maxCapacity + ", pubName=" + pubName + ", currCapacity=" + currCapacity
+				+ ", beerLiterLimit=" + beerLiterLimit + ", maxBeerLimit=" + maxBeerLimit + ", visitorsQueue="
+				+ visitorsQueue + ", visitors=" + visitors + ", visitorsQueueSize=" + visitorsQueueSize + ", drunkBeer="
+				+ drunkBeer + "]";
 	}
 
 }
--- src/main/java/webspringpub/controller/BaseController.java
+++ src/main/java/webspringpub/controller/BaseController.java
@@ -1,16 +1,7 @@
 package webspringpub.controller;
 
-import java.io.BufferedWriter;
-import java.io.ByteArrayInputStream;
-/*import java.io.ByteArrayInputStream;*/
-import java.io.IOException;/*
-import java.io.InputStream;
-import java.io.OutputStream;*/
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -19,8 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-/*import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServlet;*/
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -32,12 +22,6 @@ public class BaseController {
 	
 	@RequestMapping(value="puby", method=RequestMethod.GET)
 	public String loadPub() {
-		
-/*		pub = new Pub("Blue Lagoone", 500.0);
-		//model.addAttribute("obj", pub);
-		model.addFlashAttribute("pub", pub);
-		model.addFlashAttribute("message","Added successfully.");
-		System.out.println(model.getFlashAttributes());*/
 		return "pub";
 	}
 	
@@ -52,35 +36,32 @@ public class BaseController {
 	}
 	
 	@RequestMapping(value="add", method=RequestMethod.POST)
-	public String addVisitor(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, final RedirectAttributes model) throws IOException {
-		pub.addRandomVisitor();
-//		pub.userFilter();
+	public String addVisitor(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, 
+			final RedirectAttributes model) throws IOException, InterruptedException {
+		pub.addRandomVisitorToBar();
 		return "pub";
 	}
 	
 	@RequestMapping(value="add2queue", method=RequestMethod.POST)
-	public String addToQueue(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, final RedirectAttributes model) throws IOException {
+	public String addToQueue(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, 
+			final RedirectAttributes model) throws IOException {
 		pub.addRandomVisitorToTheQueue();
 		return "pub";
 	}
 	
 	@RequestMapping(value="processQueue", method=RequestMethod.POST)
-	public String processQueue(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, final RedirectAttributes model) throws IOException {
-		pub.userFilter();
+	public String processQueue(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, 
+			final RedirectAttributes model) throws IOException, InterruptedException {
+		pub.processTheQueue();
 		return "pub";
 	}
 	
 	@RequestMapping(value="remove", method=RequestMethod.POST)
-	public String removeVisitor(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, final RedirectAttributes model) throws IOException {
+	public String removeVisitor(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, 
+			final RedirectAttributes model) throws IOException {
 		pub.removeVisitor();
 		return "pub";
 	}
-	
-/*	@RequestMapping(value="save", method=RequestMethod.POST)
-	public String saveHistory(@ModelAttribute("pub") Pub pub, final BindingResult mapping1BindingResult, final RedirectAttributes model) throws IOException {
-		pub.saveHistoryToFile();
-		return "pub";
-	}*/
 	
 	@ModelAttribute("pub")
     public Pub getPub() {
